01251dbdf91f59cf632b32eebd8b43ad58bbb03b
KARAKI Diana <DK273056@intra.cea.fr>
2025-03-12 11:35:53 +0100

    v10

--- R/exporting_fcts.R
+++ R/exporting_fcts.R
@@ -1,17 +1,16 @@
 # the following function, extract for every identified pure source in all iterations one Spectra object;
 # this object contains 1 MS1 pure spectra and the 10 MS2 pure spectra from different isolation windows.
 
-#' Export MS pure spectra objects
+
+
+#' Title
 #'
-#' @param features.l `list` created by `DIANMF::DIANM.f`
+#' @param features.l `list`
 #'
-#' @return `Spectra` object of MS1 pure spectra.
+#' @returns `list` of `Spectra` objects
 #' @export
 #' 
-#' @import dplyr
 #' @importFrom Spectra Spectra
-#' @importFrom BiocGenerics Reduce
-#' @importFrom S4Vectors DataFrame
 exportMSSpectra <- function(features.l) {
   
   MSSpectra <- lapply(seq_along(features.l), function(iteration) {
@@ -75,45 +74,3 @@ exportMSSpectra <- function(features.l) {
   MSSpectra <- unlist(MSSpectra, recursive = FALSE)
   return(MSSpectra)
 }
-# pure_spectra <- exportMSSpectra(features.l) 
-# 
-# # msSpectra <- pure_spectra[[1]]
-# library(MsBackendMgf )
-# export(msSpectra, backend = MsBackendMgf(), file = "./output_spectra.mgf")
-# sps <- msSpectra 
-# spectraVariables(sps) # all 
-# msLevel(sps)
-# rtime(sps)
-# mz(sps)
-# intensity(sps)
-# 
-# ms1_Spectra <- filterMsLevel(msSpectra, 1L)
-# ms2_Spectra <- filterMsLevel(msSpectra, 2L)
-# 
-# ms2_spec <- filterIsolationWindow(ms2_Spectra, mz = 115)
-# mz(ms2_spec)
-# intensity(ms2_spec)
-
-
-
-# for every feature (or peak), we want one Spectra object, alot of redundancy here
-# exportMSFeaturesSpectra <- function(ms1_features.df, features.l){
-#   
-#   res <- lapply(ms1_features.df$featureid, function(f_id){
-#     # f_id <- "FT00001"
-#     print(f_id)
-#     iter <- ms1_features.df[featureid == f_id, ]$iteration %>%
-#       strsplit(",") %>%               
-#       unlist() %>%                     
-#       as.numeric() %>%                
-#       .[1]
-#     
-#     if( iter != 0 ){
-#       sub_info <- features.l[[iter]]
-#       
-#       feature_msSpectra <- exportMSSpectra(features.l = list(sub_info))
-#     }
-#   })
-#   
-#   return(res)
-# }
